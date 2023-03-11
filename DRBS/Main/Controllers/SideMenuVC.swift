e1b4ca7f71b90d9fa45e89972c71a4c6c17a96b6
kes3035 <kes3035@gmail.com>
2023-03-11 19:01:09 +0900

    feat: 사이드메뉴추가

---
+++ DRBS/Main/Controllers/SideMenuVC.swift
@@ -0,0 +1,29 @@
+//
+//  SideMenuVC.swift
+//  DRBS
+//
+//  Created by 김은상 on 2023/03/11.
+//
+
+import UIKit
+
+class SideMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DRBS/Main/View/SideMenuCell.swift
@@ -0,0 +1,30 @@
+//
+//  SideMenuCell.swift
+//  DRBS
+//
+//  Created by 김은상 on 2023/03/11.
+//
+
+import UIKit
+
+class SideMenuCell: UITableViewCell {
+    //MARK: - Properties
+
+    //MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    //MARK: - Helpers
+    
+    //MARK: - Actions
+
+
+}
