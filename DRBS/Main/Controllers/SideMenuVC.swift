2a2b7835df42f8a8b1b85de487b066716a1b8b84
kes3035 <kes3035@gmail.com>
2023-03-11 20:36:41 +0900

    feat: 커스텀사이드메뉴추가

--- DRBS/Main/Controllers/SideMenuVC.swift
+++ DRBS/Main/Controllers/SideMenuVC.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+protocol SideMenuVCDelegate: AnyObject {
+    func selectedCell(_ row: Int)
+}
 
 class SideMenuVC: UIViewController {
+    //MARK: - Properties
+
+    @IBOutlet weak var sideMenuWelcomeLabel: UILabel!
+    
+    @IBOutlet weak var sideMenuTitleLabel: UILabel!
+    
+    @IBOutlet weak var sideMenuUserIamgeView: UIImageView!
+    
+    @IBOutlet weak var sideMenuUserNameLabel: UILabel!
+    
+    @IBOutlet weak var sideMenuTableView: UITableView!
+    
+    private var menu: [SideMenuModel] = [
+    SideMenuModel(icon: UIImage(systemName: "map")!, title: "지역 설정"),
+    SideMenuModel(icon: UIImage(systemName: "wand.and.stars")!, title: "메뉴 추천"),
+    SideMenuModel(icon: UIImage(systemName: "dollarsign")!, title: "나눠 내기"),
+    SideMenuModel(icon: UIImage(systemName: "gearshape")!, title: "설정")
+    ]
+    var defaultHighlightedCell: Int = 0
+
+    weak var sideVCDelegate: SideMenuVCDelegate?
+    //MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        setupHighlightedCell()
+    }
+    //MARK: - Helpers
+    func configureUI() {
         
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView() {
+        sideMenuTableView.register(SideMenuCell.self, forCellReuseIdentifier: "SideMenuCell")
+        self.sideMenuTableView.delegate = self
+        self.sideMenuTableView.dataSource = self
+        self.sideMenuTableView.separatorStyle = .none
+        self.sideMenuTableView.rowHeight = 75
     }
-    */
+    
+    func setupHighlightedCell() {
+        DispatchQueue.main.async {
+            let defaultRow = IndexPath(row: self.defaultHighlightedCell, section: 0)
+            self.sideMenuTableView.selectRow(at: defaultRow, animated: false, scrollPosition: .none)
+        }
+    }
+    //MARK: - Actions
 
+
+}
+
+//MARK: - UITableViewDelegate
+extension SideMenuVC: UITableViewDelegate {
+    
+}
+
+//MARK: - UITableViewDataSource
+
+extension SideMenuVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SideMenuCell", for: indexPath) as! SideMenuCell
+        cell.iconImageView.image = self.menu[indexPath.row].icon
+        cell.titleLabel.text = self.menu[indexPath.row].title
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.sideVCDelegate?.selectedCell(indexPath.row)
+    }
+    
+    
 }
